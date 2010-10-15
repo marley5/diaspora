d14855372a632e48686988736d76b44050286d4f
Alec Leamas <leamas.alec@gmail.com>
2010-10-15 17:42:10 +0200

    Add a configuration file
    Adds a config file with environment and default thin
    options

---
+++ config/server.sh
@@ -0,0 +1,10 @@
+#
+# Included by script/server
+#
+PORT=3000
+
+# See thin -h for possible values.
+DEFAULT_THIN_ARGS=""
+
+# Uncomment to run in production mode.
+#export RAILS_ENV="production rails server"
